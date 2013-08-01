3e24e7e462b06e585e8519d8144501e85b9ce151
Sam Mason <sam.jbmason@gmail.com>
2013-08-01 10:44:10 +0100

    Remove instance varaible not needed

--- psdfonts.rb
+++ psdfonts.rb
@@ -1,9 +1,9 @@
 class Psdfonts < Sinatra::Application
 	get '/' do
+
 		psd = PSD.new('./assets/img/2.psd')
 		psd.parse!
-		psdHash = psd.tree.to_hash
-		@psd = "HI"
+		@psdHash = psd.tree.to_hash
 
 
 		erb :index
