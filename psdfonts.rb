71d3e0101a7f619119ee3d09c263805491018ce6
Sam Mason <sam.jbmason@gmail.com>
2013-08-06 20:00:22 +0100

    Got shit done, this writes it all
    Check if I need to use instance variable within do block

--- psdfonts.rb
+++ psdfonts.rb
@@ -1,7 +1,15 @@
 class Psdfonts < Sinatra::Application
 	get '/' do
+		erb :index
+	end
 
-		psd = PSD.new('./assets/img/2.psd')
+	post '/' do
+		Tempfile.open(['psd', '.psd']) do |psd|
+			psd.write(params[:psd][:tempfile].read)
+			@tmpPath = psd.path
+		end
+
+		psd = PSD.new(@tmpPath)
 		psd.parse!
 		psdHash = psd.tree.to_hash
 
@@ -10,16 +18,7 @@ class Psdfonts < Sinatra::Application
   			unwrap(x)
 			end.compact!.flatten!
 		@singleFonts =  singleFonts.map {|x| x[:name]}.uniq
-
-		erb :index
-	end
-
-	post '/' do
-		@title = params[:title]
-		File.open('uploads/'+params[:psd][:filename], 'w') do |psd|
-			psd.write(params[:psd][:tempfile].read)
-		end
-		@fileName = params[:psd]
+		
 
 
 		erb :index
