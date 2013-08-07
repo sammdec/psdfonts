639ea11d59febedeea952ed5c7fcd478bdd52500
Sam Mason <sam.jbmason@gmail.com>
2013-08-07 12:35:05 +0100

    Sorted Where this is supoosed to be and some serverside val

--- psdfonts.rb
+++ psdfonts.rb
@@ -4,24 +4,30 @@ class Psdfonts < Sinatra::Application
 	end
 
 	post '/' do
-		Tempfile.open(['psd', '.psd']) do |psd|
-			psd.write(params[:psd][:tempfile].read)
-			@tmpPath = psd.path
+		@uploadedFile = params[:psd]
+		if @uploadedFile.nil?
+			erb :index
+		else
+			Tempfile.open(['psd', '.psd']) do |file|
+				file.write(@uploadedFile[:tempfile].read)
+				@tmpPath = file.path
+			end
+
+			psd = PSD.new(@tmpPath)
+			psd.parse!
+			psdHash = psd.tree.to_hash
+
+			singleFonts = 
+				key_occurences(psdHash, :font).map! do |x| 
+	  			unwrap(x)
+				end.compact!.flatten!
+
+			@singleFonts =  singleFonts.map {|x| x[:name]}.uniq
+
+			erb :results
 		end
 
-		psd = PSD.new(@tmpPath)
-		psd.parse!
-		psdHash = psd.tree.to_hash
-
-		singleFonts = 
-			key_occurences(psdHash, :font).map! do |x| 
-  			unwrap(x)
-			end.compact!.flatten!
-		@singleFonts =  singleFonts.map {|x| x[:name]}.uniq
 		
-
-
-		erb :index
 	end
 
 end
