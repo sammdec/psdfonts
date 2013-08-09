a1290827285046398855ccc3dc362eba9b2f93a6
Sam Mason <sam.jbmason@gmail.com>
2013-08-09 18:34:24 +0100

    Got js working as needed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,33 @@
-new FormValidator('psd-form', [{
-	name: 'psd',
-	display: 'PSD file',
-	rules: 'required'
-}], function(errors, evt) {
-    var errorWrap = $('.errors-box');
-    if (errors.length > 0) {
+$(document).ready(function(){
+    // This blocks the button default event and fires the field input but allows for styling.
+    $('#js-file-btn').click(function(e){
+        e.preventDefault();
+        $('#js-file-field').click();
+    });
 
-        var errorString = '';
+    new FormValidator('psd-form', [{
+    name: 'psd',
+    display: 'PSD file',
+    rules: 'required'
+    }], function(errors, evt) {
+        var errorWrap = $('.errors-box');
+        if (errors.length > 0) {
 
-        for (var i = 0, errorLength = errors.length; i < errorLength; i++) {
-            errorString += errors[i].message + '<br />';
+            var errorString = '';
+
+            for (var i = 0, errorLength = errors.length; i < errorLength; i++) {
+                errorString += '<p class="errors-box__message">' + errors[i].message + '<p>';
+            }
+
+            errorWrap.html(errorString);
         }
 
-        errorWrap.innerHTML = errorString;
-    }
-
-    if (evt && evt.preventDefault) {
-    	evt.preventDefault();
-    } else if (event) {
-        event.returnValue = false;
-    }
+        if (evt && evt.preventDefault) {
+            evt.preventDefault();
+        } else if (event) {
+            event.returnValue = false;
+        }
+    });
 });
+
+
