beb15e6fe60257d96d9f92ab255c512854022dad
Sam Mason <sam.jbmason@gmail.com>
2013-08-17 13:19:47 +0100

    Separate out vars into neat sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,26 +2,28 @@ $(document).ready(function(){
     // This blocks the button default event and fires the field input but allows for styling.
 
     var realFileField = $('#js-file-field');
+    var errorWrap = $('.errors-box');
 
     $('#js-file-btn').click(function(e){
         e.preventDefault();
         realFileField.click();
+        errorWrap.removeClass('fade-in');
     });
 
     realFileField.change(function(){
         var filePath = $(this).val();
         var fileName = filePath.substr(filePath.lastIndexOf('\\')+1);
 
-        $('#js-file-name').html(fileName).addClass('fade-in');
-        $('.start-again').addClass('fade-in');
+        $('#js-file-name').text(fileName);
+        $('.upload-form__file-actions').addClass('fade-in');
     });
 
-    new FormValidator('psd-form', [{
+   formValidator = new FormValidator('psd-form', [{
     name: 'psd',
-    display: 'PSD file',
+    display: 'psd file',
     rules: 'required'
     }], function(errors, evt) {
-        var errorWrap = $('.errors-box');
+
         if (errors.length > 0) {
 
             var errorString = '';
@@ -30,20 +32,18 @@ $(document).ready(function(){
                 errorString += '<p class="errors-box__message">' + errors[i].message + '<p>';
             }
 
-            errorWrap.html(errorString);
-            setTimeout(function(){
-                errorWrap.fadeOut('fast', function(){
-                    errorWrap.empty();
-                });
-            },3000);
-        }
-        // This stops the form from firing if there are errors
-        if (evt && evt.preventDefault) {
-            evt.preventDefault();
-        } else if (event) {
-            event.returnValue = false;
-        }
+            errorWrap.html(errorString).addClass('fade-in');
+
+            // This stops the form from firing if there are errors
+            if (evt && evt.preventDefault) {
+                evt.preventDefault();
+            } else if (event) {
+                event.returnValue = false;
+            };
+        };
     });
+
+    formValidator.setMessage('required', 'You must choose a %s.');
 });
 
 
