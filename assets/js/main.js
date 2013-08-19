ff3d3e410cfc6af0b17aad7b3067e6caa561c893
Sam Mason <sam.jbmason@gmail.com>
2013-08-19 11:55:22 +0100

    Ipdate clipboard copy state

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,14 +50,20 @@ $(document).ready(function(){
     formValidator.setMessage('required', 'You must choose a %s file');
     formValidator.setMessage('is_file_type', 'The file must be a %s');
 
-
-    var clip = new ZeroClipboard( $('#js-copy-btn'), {
+    var clipButton = $('#js-copy-btn');
+    var clip = new ZeroClipboard( clipButton, {
         moviePath: '/assets/zero-clipboard.swf',
         hoverClass: 'clipboard-is-hover'
     });
 
     clip.on('complete', function(client, args) {
-        alert('Copied!');
+        var originalText = clipButton.text();
+        var copiedText = clipButton.data('copy-text');
+
+        clipButton.text(copiedText).addClass('attn-grab');
+        setTimeout(function(){
+            clipButton.removeClass('attn-grab');
+        },6000);
     });
 });
 
