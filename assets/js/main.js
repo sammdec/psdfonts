cac3c1b76bd01d81625f07a7a0e7d0331e527d90
Sam Mason <sam.jbmason@gmail.com>
2014-02-07 09:42:48 +0000

    Use new js files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,67 +1,48 @@
 $(document).ready(function(){
 
-    $('#js-file-dropbox').click(function(e){
+    var dropboxBtn = $('#js-file-dropbox'),
+        dropboxFile = $('#js-dropbox-field'),
+        uploadForm = $('.upload-form'),
+        flash = $('.js-flash'),
+        clipButton = $('#js-copy-btn');;
+
+
+    dropboxBtn.click(function(e){
         e.preventDefault();
-        GoSquared.DefaultTracker.TrackEvent('Upload method - Dropbox');
+        //GoSquared.DefaultTracker.TrackEvent('Upload method - Dropbox');
         Dropbox.choose({
-            linkType: "direct",
+            linkType: 'direct',
             multiselect: false,
             extensions: ['.psd'],
             success: function(files) {
-                $('#js-dropbox-field').val(files[0].link).valid();
-                fadeInName(files[0].name);
+                dropboxFile.val(files[0].link);
+                dropboxBtn.addClass('added');
             }
         });
     });
 
-   $('.upload-form').validate({
-    ignore: "",
-    errorLabelContainer: '.errors-box',
-    errorElement: 'p',
-    errorClass: 'errors-box__message',
-    highlight: function(element, errorClass, validClass) {
-        $('.errors-box').show(0, function(){
-            $(this).addClass('fade-in');
-        });
-    },
-    rules: {
-        dropbox_file: {
-            required: true
+    uploadForm.on('submit', function() {
+        if(dropboxFile.val().length != 0) {
+            $('.upload-form__btn--submit').html('uploading...').addClass('uploading');
+        } else {
+            $('.js-flash').fadeIn().delay(3000).fadeOut();
+            return false;
         }
-    },
-    messages: {
-        dropbox_file: {
-            required: "You need to choose a psd"
-        }
-    },
-    submitHandler: function(form) {
-        form.submit();
-        $('.upload-form__btn--submit').html('uploading <i></i>');
-        GoSquared.DefaultTracker.TrackEvent('Start Upload');
-    }
-   });
-
-   var clipButton = $('#js-copy-btn');
-    var clip = new ZeroClipboard( clipButton, {
-        moviePath: '/zero-clipboard.swf',
-        hoverClass: 'clipboard-is-hover'
     });
 
+
+
+    ZeroClipboard.config({ moviePath: '/ZeroClipboard.swf' });
+
+    var clip = new ZeroClipboard(clipButton);
+
     clip.on('complete', function(client, args) {
-        var originalText = clipButton.text();
-        var copiedText = clipButton.data('copy-text');
-
-        clipButton.text(copiedText).addClass('attn-grab');
-        setTimeout(function(){
-            clipButton.removeClass('attn-grab');
-        },6000);
-
-        GoSquared.DefaultTracker.TrackEvent('Upload another');
+        clipButton.html('copied!');
     });
 
     // Event for seeing if a user goes to buy a font
     $('.font-list__buy-font').click(function(){
-        _gs('event', 'Font buy link clicked');
+        //_gs('event', 'Font buy link clicked');
     });
 });
 
